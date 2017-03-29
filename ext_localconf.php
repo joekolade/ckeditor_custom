8006cdae000736ee0de6553f5e52925b9cbe01e9
Joe Schäfer <schaefer@scw.info>
2017-03-29 09:59:16 +0200

    initial commit dev

---
+++ ext_localconf.php
@@ -0,0 +1,8 @@
+<?php
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ckeditor_custom/Configuration/TSconfig/page.ts">'
+);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
+    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ckeditor_custom/Configuration/TSconfig/user.ts">'
+);
