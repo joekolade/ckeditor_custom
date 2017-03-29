4a625c2784e5c894f25c25e2701bd768a27c5991
Joe Schäfer <schaefer@scw.info>
2017-03-29 10:01:18 +0200

    Bind TS und co

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,3 +6,7 @@
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
     '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ckeditor_custom/Configuration/TSconfig/user.ts">'
 );
+
+
+// Set presets
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['test_preset'] = 'EXT:ckeditor_custom/Configuration/RTE/Test.yaml';
